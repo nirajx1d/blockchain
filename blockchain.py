1748963983157a6fa4635254f8341c9fbf44ca68
nirajx1d <32599550+nirajx1d@users.noreply.github.com>
2019-02-02 21:05:03 +0530

    get full blockchain

--- blockchain.py
+++ blockchain.py
@@ -68,7 +68,7 @@ blockchain = Blockchain()
 @app.route('/mine_block', methods = ['GET'])
 def mine_block():
     previous_block = blockchain.get_previous_block()
-    previous_proof = previous_block['proof]
+    previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
     block = blockchain.create_block(proof, previous_hash)
@@ -78,26 +78,14 @@ def mine_block():
                 'proof': block['proof'],
                 'previous_hash': block['previous_hash']}
     return jsonify(response), 200
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+#getting the full blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+#running the app
+app.run(host = '0.0.0.0', port = 5000)
     
