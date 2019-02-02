d45496f608b1f4163ca4e7469a08ce5546f5ec1f
nirajx1d <32599550+nirajx1d@users.noreply.github.com>
2019-02-02 19:48:52 +0530

    proof of work

--- blockchain.py
+++ blockchain.py
@@ -19,5 +19,14 @@ class Blockchain:
                  'timestamp': str(datetime.datetime.now()),
                  'proof': proof,
                  'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+    def get_previous_block(self):
+        return self.chain[-1]
 
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation =hashlib.sha256(str(new_proof**2 - previous_proof**2))
 #part 2- mining our blockchain
