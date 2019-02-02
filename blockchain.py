916a7b72dc52d1231f1fc6b4e33da44273efa55c
nirajx1d <32599550+nirajx1d@users.noreply.github.com>
2019-02-02 18:32:22 +0530

    create block

---
+++ blockchain.py
@@ -0,0 +1,23 @@
+
+#importing the libraries
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#part 1- build a blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+    
+    def create_block(self, proof, previous_hash):
+        block = {'index':len(self.chain) + 1, 
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+
+#part 2- mining our blockchain
